0e7ccdc56fc475996d5d85401014f117b32b7190
Veries Seals <veries@me.com>
2025-07-20 15:29:40 -0500

    Corrected urlpatterns under project urls.py

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
+# The urlpatterns list routes URLs to views.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
